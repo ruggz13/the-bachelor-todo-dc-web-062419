3eb6e5984da21ee8d7d1adde95732290e1489b76
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-24 18:03:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,8 +4,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season, attributes|
     if attributes == "status".downcase == "winner"
       return attributes["name"].split(" ").first
-
-
+    end
   end
 end
 
