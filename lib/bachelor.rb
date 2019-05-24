9c1dd3f85fb1379df432180eae1affa64975e9ee
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-24 18:04:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,8 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |season, attributes|
-    if attributes == "status".downcase == "winner"
+  data[season].each do |contestant_hash|
+    if contestant_hash["status"].downcase == "winner"
       return attributes["name"].split(" ").first
     end
   end
