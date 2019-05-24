49f7ae46816530cb90440eda4ad468b607e6064a
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-24 18:12:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,7 @@ def get_contestant_name(data, occupation)
     contestants.each do |contestant_hash|
       if contestant_hash["occupation"] == occupation
         return contestant_hash["name"]
+      end
     end
   end
 end
