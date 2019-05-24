50aad41d019e38d7d1c439f9ef69ee8cdbd555c8
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-24 18:29:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,6 +50,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant_hash|
       age_total += (contestant_hash["age"]).to_i
       num_contestants += 1
+      binding.pry
     end
     (age_total / num_contestants.to_f).round(0)
     #binding.pry
